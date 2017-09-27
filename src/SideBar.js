774b9ec8341fa2528a49c7c91c82222f5fb19062
Steve Brennan <brennanstevenp@gmail.com>
2017-09-27 15:04:02 +1000

    Styling sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -4,41 +4,51 @@ import {Flex, Box} from 'grid-styled';
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    NavLink
   } from 'react-router-dom';
 
-const NavLink = styled(Link)`
+const SideNavLink = styled(NavLink)`
     color: red;
     text-decoration: none;
-    margin: 0px;
     height: 100%;
     width: 100%;
-    padding-top: 40px;
+    border-right: 1px solid grey;
+    &.bio-link, &.timeline-link, &.skills-link, &.projects-link {
+         background-color: white;
+    }
+    &.active {
+        border-bottom: 1px solid grey;
+        border-top: 1px solid grey;
+        border-right: 0px;
+
+        &.intro-link {
+            border-top: 0px;
+            border-bottom: 1px solid grey;
+        }
+    }
 `;
 
 const NavList = styled.ul`
-  list-style-type: none;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: top;
-  margin: 0;
-  padding: 0;
+    list-style-type: none;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    margin: 0;
+    padding: 0;
 `;
 
 const NavListItem = styled.li`
-    border: 1px solid blue;
     text-align: center;
-    padding: 0px;
     margin: 0 auto;
     display: flex;
-    height: 100px;
+    height: 100%;
     width: 100px;
 `;
 
 const NavSection = styled.section`
-  border: 1px solid black;
   display: flex;
   height: 100%;
   margin: 0;
@@ -49,16 +59,35 @@ const NavSection = styled.section`
 
 
 class SideBar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleNavigationChange = this.handleNavigationChange.bind(this);
+        this.state = {currentView: 'intro'};
+    }
+
+    handleNavigationChange(e) {
+        this.setState({currentView:e.target.firstChild.textContent.toLowerCase()});
+    }
 
     render() {
         return (
             <NavSection className="nav-section">
                     <NavList className="nav-list">
-                        <NavListItem><NavLink to="/">Intro</NavLink></NavListItem>
-                        <NavListItem><NavLink to="/bio">Bio</NavLink></NavListItem>
-                        <NavListItem><NavLink to="/timeline">Timeline</NavLink></NavListItem>
-                        <NavListItem><NavLink to="/skills">Skills</NavLink></NavListItem>
-                        <NavListItem><NavLink to="/projects">Projects</NavLink></NavListItem>
+                        <NavListItem>
+                            <SideNavLink exact to="/" className="intro-link">Intro</SideNavLink>
+                        </NavListItem>
+                        <NavListItem>
+                            <SideNavLink exact to="/bio" className="bio-link">Bio</SideNavLink>
+                        </NavListItem>
+                        <NavListItem>
+                            <SideNavLink exact to="/timeline" className="timeline-link">Timeline</SideNavLink>
+                        </NavListItem>
+                        <NavListItem>
+                            <SideNavLink exact to="/skills" className="skills-link">Skills</SideNavLink>
+                        </NavListItem>
+                        <NavListItem>
+                            <SideNavLink exact to="/projects" className="projects-link">Projects</SideNavLink>
+                        </NavListItem>
                     </NavList>
             </NavSection>
         );
